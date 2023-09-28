db2db8d2f79f5d1364172139251d6bc3467ef922
Katia Valle <katinavalle9@gmail.com>
2023-09-28 15:32:27 -0600

    Realizando vista

---
+++ js/proyecto.js
@@ -0,0 +1,9 @@
+class Alumno{
+    constructor(nombre,apellido,edad,materiasIncritas,calificaciones){
+        this.nombre = nombre;
+        this.apellido =apellido;
+        this.edad = edad;
+        this.materiasIncritas = materiasIncritas;
+        this.calificaciones = calificaciones;
+    }
+}
